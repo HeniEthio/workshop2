8a7b5521f5ead1b2186f29dd7616989a6e07e3a2
Bob Bobbington <he4god@gmail.com>
2022-10-29 12:04:06 -0700

    update dns address

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import os 
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    ' ec2-52-13-134-176.us-west-2.compute.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1'
+    'ec2-52-13-134-176.us-west-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
 ]
 
 
